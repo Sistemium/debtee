ea337bda936c1a474d50c8d015dee07865949650
Albert <kovalevskij.albert@gmail.com>
2016-05-24 14:52:01 +0300

    remove auth api files

--- server/routes.js
+++ server/routes.js
@@ -12,8 +12,6 @@ export default function(app) {
   app.use('/api/domain', require('./api/agent'));
   app.use('/api', require('./api'));
 
-  app.use('/auth', require('./auth'));
-
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
