eb776461892df36bafeb9113e1eb26cb2af3cdea
Albert <kovalevskij.albert@gmail.com>
2016-05-25 08:31:44 +0300

    appConfig.authUrl

--- client/components/oauth-buttons/oauth-buttons.controller.js
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('authApiApp')
-  .controller('OauthButtonsCtrl', function($window) {
+  .controller('OauthButtonsCtrl', function($window, appConfig) {
     this.loginOauth = function(provider) {
-      $window.location.href = '/auth/' + provider;
+      $window.location.href = appConfig.authUrl + 'auth/' + provider;
     };
   });
